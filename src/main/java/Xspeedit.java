9044aef3bcaae0f49e447904d20ab96c0630c4b1
Francois Nollen <fnollen@oui.sncf>
2021-02-10 13:49:38 +0100

    Prepare project for basic Java impl
    Signed-off-by: Francois Nollen <fnollen@oui.sncf>

---
+++ src/main/java/Xspeedit.java
@@ -0,0 +1,13 @@
+public class Xspeedit {
+
+    /**
+     * Package articles (sizes as chars) of size maximum 10.
+     *
+     * @param articles the concatenated articles/sizes
+     */
+    public String packageArticles(String articles) {
+
+        // TODO: split articles by packages
+        return articles;
+    }
+}
---
+++ src/test/java/XspeeditTest.java
@@ -0,0 +1,9 @@
+public class XspeeditTest {
+
+    public void packaging() {
+
+        String packages = new Xspeedit().packageArticles("163841689525773");
+
+        // TODO: expect 163/8/41/6/8/9/52/5/7/73
+    }
+}
