6058d16a32bcc28027707b8db5a19caa24a8b389
Teklit <teklitte@gmail.com>
2022-07-14 23:28:13 +0200

    Set-up project

--- src/main/java/Xspeedit.java
+++ src/main/java/Xspeedit.java
@@ -1,13 +1,92 @@
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 public class Xspeedit {
 
-    /**
-     * Package articles (sizes as chars) of size maximum 10.
-     *
-     * @param articles the concatenated articles/sizes
-     */
-    public String packageArticles(String articles) {
+    static ArrayList<String> sArticles =new ArrayList<String>();//Creating arraylist 
 
-        // TODO: split articles by packages
+    public String packageArticles(String articles) {
+    
+        char[] article = articles.toCharArray();
+        // declaring an array with the size of chartter
+        int[] arr = new int[article.length]; 
+        // char object and storing that integer into the int a=char -'0'
+        for (int i = 0; i < article.length; i++) {
+            arr[i] = article[i] - '0';
+        }
+
+        getSumOfArrayList(arr);
+        articles =  arrayListToStringChange();
         return articles;
+
     }
+
+    /**
+     * separate the array by sum of 10/
+     * @param arr
+     */
+    public void  getSumOfArrayList(int[] arr){
+        int sum = 0;
+        int x=0;
+        int y=0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i]; 
+            if(sum==10 && i==arr.length-1){
+                x=y;
+                y=i+1;
+                addSeparateValue(x, y, arr);
+                sum=0;
+            }else if(sum==10){
+                y=i+1;
+                addSeparateValue(x, y, arr);
+                sum=0;
+            
+            }else if(sum<10 && i==arr.length-1){
+                x=y;
+                y=i+1;
+                addSeparateValue(x, y, arr);
+                sum=0;
+            }else{
+                int tsum = 0;
+                tsum = sum + arr[i+1];
+                if(tsum > 10){
+                    tsum = 0;
+                    x=y;
+                    y=i+1;
+                    addSeparateValue(x, y, arr);
+                    sum=0;
+                }
+            }
+        }
+
+    }
+
+
+    /**
+     * add seperator to the array list and add to static variable
+     * @param x
+     * @param y
+     * @param arr
+     */
+    public void addSeparateValue(int x, int y,  int[] arr){
+        ArrayList<String> sArticle =new ArrayList<String>(); 
+            for (int i=x; i<y; i++) {
+                sArticle.add(Integer.toString(arr[i]));
+            }
+            sArticle.add("/");
+        sArticles.addAll(sArticle);
+    }
+
+
+    /**
+     * return the final result
+     * @return String
+     */
+    public String arrayListToStringChange(){
+        String astring = sArticles.stream().map(Object::toString).collect(Collectors.joining(""));
+        astring = astring.substring(0, astring.length() - 1);
+        return astring;
+    }
+  
 }
--- src/test/java/XspeeditTest.java
+++ src/test/java/XspeeditTest.java
@@ -1,9 +1,11 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 public class XspeeditTest {
-
+    
+    @Test
     public void packaging() {
 
         String packages = new Xspeedit().packageArticles("163841689525773");
-
-        // TODO: expect 163/8/41/6/8/9/52/5/7/73
+        Assert.assertEquals(packages, "163/8/41/6/8/9/52/5/7/73");
     }
 }
